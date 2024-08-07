058f74d6fe0ae962e02c158c598677a07c154a23
melchanatan <chanatan@outlook.com>
2024-08-07 17:10:02 +0700

    feat: update dockerfile

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,5 +3,9 @@ import { db } from "@/lib/db";
 export async function GET() {
   const allUsers = await db.user.findMany();
 
-  return new Response(JSON.stringify(allUsers));
+  try {
+    return new Response(JSON.stringify(allUsers));
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
 }
