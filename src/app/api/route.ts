52bac8168db3cbd664dacffd7749cc5aa90b65f5
mels <chanatan.melmel@gmail.com>
2024-08-07 17:11:09 +0700

    feat: post request test

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,3 +5,26 @@ export async function GET() {
 
   return new Response(JSON.stringify(allUsers));
 }
+
+export async function POST(params) {
+  await db.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+
+  const allUsers = await db.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
